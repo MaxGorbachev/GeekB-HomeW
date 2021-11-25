cddd1e8ec148019da86dc1e0668b850e3a6c5620
Max Gorbachev <gorbachyov.maksim@mail.ru>
2021-11-25 19:34:29 +0300

    hw2

--- GeekB HomeW/main.swift
+++ GeekB HomeW/main.swift
@@ -1,66 +1,60 @@
 //  Created by Макс on 19.11.2021.
 import Foundation
 
-/* Решение квадратного уравнения */
-print("Предлагается решить уравнение формата ax^2 + bx + c = 0 с заданными Вами коэффициентами a, b и c.\n")
-print("Введите коэффициент а: ")
-let userAnsA = readLine()
-print("Введите коэффициент b: ")
-let userAnsB = readLine()
-print("Введите коэффициент c: ")
-let userAnsC = readLine()
-var a: Double = 0; var b: Double = 0; var c: Double = 0
-if userAnsA != nil && userAnsB != nil && userAnsC != nil { // Проверка на наличие значения и unwrap optional
-    a += Double(userAnsA!)!
-    b += Double(userAnsB!)!
-    c += Double(userAnsC!)!
-} else {
-    print("Error")
-}
-var d: Double = (b * b) - (4 * a * c) // Подсчёт дискриминанта
-if d < 0 {
-    print("Корни отсутствуют")
-} else if (d == 0) {
-    let x1 = (-b + d) / 2 * a
-    print("При данных коэффициентах мы получаем нулевой дискриминант, следовательно оба корня будут равны:\(x1).")
-} else {
-    let x1 = (-b + sqrt(d)) / 2 * a
-    let x2 = (-b - sqrt(d)) / 2 * a
-    print("Первый корень равен: \(x1), а второй: \(x2). \n \n")
+/* Решение задачи с определением чётности числа */
+func evenCheck(number: Int) -> Bool {
+    if number % 2 == 0 {
+        return true
+    } else {
+        return false
+    }
 }
 
-/* Решение задачи с треугольником */
-print("Предлагается ввести два катета треугольника для получения гиппотенузы, периметра, и площади треуголька.")
-print("Введите первый катет треугольника: ")
-let userCat1 = readLine()
-print("Введите второй катет треугольника: ")
-let userCat2 = readLine()
-var cat1: Double = 0; var cat2: Double = 0
-if userCat1 != nil && userCat2 != nil { // Проверка на наличие значения и unwrap optional
-    cat1 += Double(userCat1!)!
-    cat2 += Double(userCat2!)!
-} else {
-    print("Error")
+/* Решение задачи с делимостью на три без остатка */
+func delThreeCheck(number: Int) -> Bool {
+    if number % 3 == 0 {
+        return true
+    } else {
+        return false
+    }
 }
-let gippoten = round(sqrt(cat1 * cat1 + cat2 * cat2))
-let perimetr = round(cat1 + cat2 + gippoten)
-let square = 0.5 * cat1 * cat2
-print("С данными катетами гиппотенуза будет равна: \(gippoten), площадь: \(square), а периметр: \(perimetr). \n \n")
 
-/* Решение банковской задачи */
-print("Предлагается рассчитать сумму дохода по банковскому вкладу с заданным процентом через 5 лет.")
-print("Введите первоначальную сумму вклада:")
-let userCashAmount = readLine()
-print("Введите ежегодный процент по вкладу:")
-let userPercentAmount = readLine()
-var cashAmount: Double = 0; var percentAmount: Double = 0
-if userCashAmount != nil && userPercentAmount != nil { // Проверка на наличие значения и unwrap optional
-    cashAmount += Double(userCashAmount!)!
-    percentAmount += Double(userPercentAmount!)! / 100
-} else {
-    print("Error")
+/* Решение задаачи с созданием возрастающего массива из 100 чисел */
+func arrayGenerate(length: Int) -> Array<Int> {
+    var numbersArray = [Int]()
+    var numberForArray = 0
+    for _ in 1...length {
+        numbersArray.append(numberForArray)
+        numberForArray += 1
+    }
+
+    return numbersArray
 }
-for _ in 1...5 {
-    cashAmount += cashAmount * percentAmount
+
+var numbersArray = arrayGenerate(length: 100)
+
+
+/* Решение задачи с удалением из массива чётные числа и числа кратные трём */
+numbersArray = numbersArray.filter({($0 % 2 != 0) && ($0 % 3 == 0)})
+
+/* Решение задачи с числами Фибоначчи */
+var fibonacciArr = [0, 1]
+var newFibNum = 1
+var oldFibNum = 1
+
+for _ in 3...50 {
+    fibonacciArr.append(newFibNum)
+    let numTransfer = newFibNum
+    newFibNum += oldFibNum
+    oldFibNum = numTransfer
 }
-print("Через 5 лет на вашем счету будет: \(round(cashAmount)).")
+
+/* Решение шестой задачи */
+var simpleArray = [Int](1...100)
+let maxOfArray = simpleArray.max()!
+var p = 2
+while p * p < maxOfArray {
+    simpleArray = simpleArray.filter({$0 % p != 0})
+    p += 1
+}
+print(simpleArray)
