1f9bf237f4351ec56fce24a8e284b89a395f4eda
Max Gorbachev <gorbachyov.maksim@mail.ru>
2021-12-03 00:40:53 +0300

    hw4 done

--- GeekB HomeW/main.swift
+++ GeekB HomeW/main.swift
@@ -1,32 +1,29 @@
-/// Структура, описывающая объект легкового автомобиля,
-/// включает в себя ряд конфигурационных свойств для деталей данного объекта.
-struct SportCar {
-
-    enum EngineOn {
+// MARK: - Enums
+    enum EngineOn: String {
 
         case isOn
         case isOff
     }
 
-    enum WindowsStatus {
+    enum WindowsStatus: String {
 
         case opened
         case closed
     }
 
-    enum TrunkStatus {
+    enum TrunkStatus: String {
 
         case opened
         case closed
     }
 
-    enum DoorsStatus {
+    enum DoorsStatus: String {
 
         case opened
         case closed
     }
 
-    enum LightsStatus {
+    enum LightsStatus: String {
 
         case on
         case off
@@ -38,12 +35,15 @@ struct SportCar {
         case unload(amount: UInt)
     }
 
-    let carBrand: String
-    let yearOfProd: UInt
-    let engineVolume: Float
-    let carMaxSpeed: UInt
+// MARK: - Classes
+
+class Car {
+    var carBrand: String
+    var yearOfProd: UInt
+    var engineVolume: Float
+    var carMaxSpeed: UInt
     static let carPossibleSpeed: UInt = 300
-    let trunkCapacity: UInt
+    var trunkCapacity: UInt
     var trunkFullness: UInt = 0
     var windowsOpened: WindowsStatus = .closed
     var doorsOpened: DoorsStatus = .closed
@@ -51,7 +51,11 @@ struct SportCar {
     var lightsOn: LightsStatus = .off
     var engineOn: EngineOn = .isOff
 
-    init(carBrand: String, yearOfProd: UInt, engineVolume: Float, carMaxSpeed: UInt, trunkCapacity: UInt,
+    init(carBrand: String,
+         yearOfProd: UInt,
+         engineVolume: Float,
+         carMaxSpeed: UInt,
+         trunkCapacity: UInt,
          trunkFullness: UInt = 0) {
 
         self.carBrand = carBrand
@@ -60,242 +64,115 @@ struct SportCar {
         self.trunkCapacity = trunkCapacity
         self.trunkFullness = trunkFullness
 
-        if carMaxSpeed > SportCar.carPossibleSpeed {
+        if carMaxSpeed > Car.carPossibleSpeed {
 
             print("Введено недопустимое значение. Установлено максимально возможное значение: 300 км/ч.")
-            self.carMaxSpeed = SportCar.carPossibleSpeed
+            self.carMaxSpeed = Car.carPossibleSpeed
 
         } else {
 
             self.carMaxSpeed = carMaxSpeed
 
         }
-
-        print("One of the cars, branded as: \(self.carBrand), built in \(self.yearOfProd).")
-        print("Engine volume is \(self.engineVolume), and maximum speed is: \(self.carMaxSpeed).")
-        print("Windows are \(self.windowsOpened) and doors are \(self.doorsOpened).")
-        print("Trunk capacity is \(self.trunkCapacity) with \(self.trunkFullness) used.")
     }
 
-    mutating func lightsAccess(status: LightsStatus) {
+    func lightsAccess(status: LightsStatus) {
 
-        self.lightsOn = status
+        lightsOn = status
         print("Now lights are \(status).")
 
     }
 
-    mutating func windowsAccess(status: WindowsStatus) {
+    func windowsAccess(status: WindowsStatus) {
 
-        self.windowsOpened = status
+        windowsOpened = status
         print("Now, windows are \(status).")
 
     }
 
-    mutating func doorsAccess(status: DoorsStatus) {
+    func doorsAccess(status: DoorsStatus) {
 
-        self.doorsOpened = status
+        doorsOpened = status
         print("Now, doors are \(status).")
 
     }
 
-    mutating func trunkAccess(status: TrunkStatus) {
+    func trunkAccess(status: TrunkStatus) {
 
-        self.trunkOpen = status
+        trunkOpen = status
         print("Now trunk is \(status).")
 
     }
 
-    mutating func trunkLoadness(status: TrunkFilling) {
+    func trunkLoadness(status: TrunkFilling) {
 
         switch status {
 
         case let .load(amount):
-            if (self.trunkOpen == .closed) {
+            if (trunkOpen == .closed) {
                 print("Open the trunk before loading.")
             } else if amount <= (trunkCapacity - trunkFullness) {
                 print("Car is loaded with \(amount).")
-                self.trunkFullness += amount
+                trunkFullness += amount
             } else {
                 print("Couldn't load that amount of weight.")
                 print("It's only possible to put \(trunkCapacity - trunkFullness) in.")
             }
         case let .unload(amount):
-            if self.trunkOpen == .closed {
+            if trunkOpen == .closed {
                 print("Open the trunk before unloading.")
             } else if amount >= trunkFullness {
                 print("Car is fully unloaded.")
-                self.trunkFullness = 0
+                trunkFullness = 0
             } else {
                 print("Car is unloaded with \(amount) and now is loaded with \(trunkFullness)")
-                self.trunkFullness -= amount
+                trunkFullness -= amount
             }
         }
     }
 
-    func aboutCar() {
-        print("This car is branded as: \(self.carBrand), built in \(self.yearOfProd).")
-        print("Engine volume is \(self.engineVolume), and maximum speed is: \(self.carMaxSpeed).")
-        print("Windows are \(self.windowsOpened) and doors are \(self.doorsOpened). Lights are \(self.lightsOn).")
-        print("Trunk capacity is \(self.trunkCapacity) with \(self.trunkFullness) used.")
-    }
+
 }
 
-/// Структура, описывающая объект грузового автомобиля,
-/// включает в себя ряд конфигурационных свойств для деталей данного объекта.
-struct TrunkCar {
+final class SportCar: Car {
 
-    enum EngineOn {
+func carInfo() {
+        print("One of the cars, branded as: \(carBrand), built in \(yearOfProd).")
+        print("Engine volume is \(engineVolume), and maximum speed is: \(carMaxSpeed).")
+        print("Windows are \(windowsOpened) and doors are \(doorsOpened).")
+        print("Trunk capacity is \(trunkCapacity) with \(trunkFullness) used.")
+    }
+    
+}
 
-        case isOn
-        case isOff
+
+final class TrunkCar: Car {
+
+    func aboutTruck() {
+        print("This truck is branded as: \(carBrand), builr in \(yearOfProd).")
+        print("Engine volume is \(engineVolume), and maximum speed is: \(carMaxSpeed).")
+        print("Windows are \(windowsOpened) and doors are \(doorsOpened). Lights are \(lightsOn).")
+        print("Trunk capacity is \(trunkCapacity) with \(trunkFullness) used.")
     }
 
-    enum WindowsStatus {
-
-        case opened
-        case closed
-    }
-
-    enum TrunkStatus {
-
-        case opened
-        case closed
-    }
-
-    enum DoorsStatus {
-
-        case opened
-        case closed
-    }
-
-    enum LightsStatus {
-
-        case on
-        case off
-    }
-
-    enum TrunkFilling {
-
-        case load(amount: UInt)
-        case unload(amount: UInt)
-    }
-
-    let truckBrand: String
-    let yearOfProd: UInt
-    let engineVolume: Float
-    let truckMaxSpeed: UInt
-    static let truckPossibleSpeed: UInt = 200
-    let trunkCapacity: UInt
-    var trunkFullness: UInt = 0
-    var windowsOpened: WindowsStatus = .closed
-    var doorsOpened: DoorsStatus = .closed
-    var trunkOpen: TrunkStatus = .closed
-    var lightsOn: LightsStatus = .off
-    var engineOn: EngineOn = .isOff
-
-    init(truckBrand: String, yearOfProd: UInt, engineVolume: Float, truckMaxSpeed: UInt, trunkCapacity: UInt,
-         trunkFullness: UInt = 0) {
-
-        self.truckBrand = truckBrand
-        self.yearOfProd = yearOfProd
-        self.engineVolume = engineVolume
-        self.trunkCapacity = trunkCapacity
-        self.trunkFullness = trunkFullness
-
-        if truckMaxSpeed > TrunkCar.truckPossibleSpeed {
-
-            print("Введено недопустимое значение. Установлено максимально возможное значение: 200 км/ч.")
-            self.truckMaxSpeed = TrunkCar.truckPossibleSpeed
-
-        } else {
-
-            self.truckMaxSpeed = truckMaxSpeed
-
-        }
-
-        print("One of the trucks, branded as: \(self.truckBrand), built in \(self.yearOfProd).")
-        print("Engine volume is \(self.engineVolume), and maximum speed is: \(self.truckMaxSpeed).")
-        print("Windows are \(self.windowsOpened) and doors are \(self.doorsOpened).")
-        print("Trunk capacity is \(self.trunkCapacity) with \(self.trunkFullness) used.")
-    }
-
-    mutating func lightsAccess(status: LightsStatus) {
-
-        self.lightsOn = status
-        print("Now lights are \(status).")
-
-    }
-
-    mutating func windowsAccess(status: WindowsStatus) {
-
-        self.windowsOpened = status
-        print("Now, windows are \(status).")
-
-    }
-
-    mutating func doorsAccess(status: DoorsStatus) {
-
-        self.doorsOpened = status
-        print("Now, doors are \(status).")
-
-    }
-
-    mutating func trunkAccess(status: TrunkStatus) {
-
-        self.trunkOpen = status
-        print("Now trunk is \(status).")
-
-    }
-
-    mutating func trunkLoadness(status: TrunkFilling) {
-
-        switch status {
-
-        case let .load(amount):
-            if (self.trunkOpen == .closed) {
-                print("Open the trunk before loading.")
-            } else if amount <= (trunkCapacity - trunkFullness) {
-                print("Truck is loaded with \(amount).")
-                self.trunkFullness += amount
-            } else {
-                print("Couldn't load that amount of weight.")
-                print("It's only possible to put \(trunkCapacity - trunkFullness) in.")
-            }
-        case let .unload(amount):
-            if self.trunkOpen == .closed {
-                print("Open the trunk before unloading.")
-            } else if amount >= trunkFullness {
-                print("Car is fully unloaded.")
-                self.trunkFullness = 0
-            } else {
-                print("Truck is unloaded with \(amount) and now is loaded with \(trunkFullness)")
-                self.trunkFullness -= amount
-            }
-        }
-    }
-
-    func aboutCar() {
-        print("This truck is branded as: \(self.truckBrand), builr in \(self.yearOfProd).")
-        print("Engine volume is \(self.engineVolume), and maximum speed is: \(self.truckMaxSpeed).")
-        print("Windows are \(self.windowsOpened) and doors are \(self.doorsOpened). Lights are \(self.lightsOn).")
-        print("Trunk capacity is \(self.trunkCapacity) with \(self.trunkFullness) used.")
-    }
 }
 
 var sportCar = SportCar(carBrand: "BMW", yearOfProd: 2020, engineVolume: 550, carMaxSpeed: 400, trunkCapacity: 550)
-
+sportCar.carInfo()
 sportCar.lightsAccess(status: .on)
 sportCar.doorsAccess(status: .opened)
 sportCar.trunkAccess(status: .opened)
 sportCar.trunkLoadness(status: .load(amount: 320))
+sportCar.trunkAccess(status: .closed)
 print("")
-sportCar.aboutCar()
+sportCar.carInfo()
 print("")
 
-var truck = TrunkCar(truckBrand: "MAN", yearOfProd: 2016, engineVolume: 700, truckMaxSpeed: 240, trunkCapacity: 20000)
+var truck = TrunkCar(carBrand: "MAN", yearOfProd: 2016, engineVolume: 700, carMaxSpeed: 240, trunkCapacity: 20000)
 truck.windowsAccess(status: .opened)
 truck.lightsAccess(status: .on)
 truck.trunkLoadness(status: .load(amount: 40000))
+sportCar.trunkAccess(status: .opened)
 print("")
-truck.aboutCar()
+truck.aboutTruck()
 print("")
